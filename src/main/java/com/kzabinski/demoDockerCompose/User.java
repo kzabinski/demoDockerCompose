0d52fe8cdcbdca4077553d15d3ea6bf827ccfa35
kzabinski <krzysztof.kamil.zabinski@gmail.com>
2025-11-14 10:29:51 +0100

    Update User.java

--- src/main/java/com/kzabinski/demoDockerCompose/User.java
+++ src/main/java/com/kzabinski/demoDockerCompose/User.java
@@ -23,5 +23,19 @@ public class User {
     private String name;
     private String email;
 
-    // Gettery i settery
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
